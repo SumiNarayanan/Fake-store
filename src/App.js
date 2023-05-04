7dff02d1c1f1bbde8dff95989f5fb241fd2da25e
Sumi <suminarayanan@outlook.com>
2023-05-04 12:07:47 +0400

    completed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Fakestore from "./component/Fakestore";
+
+function App() {
+  return (
+    <div className="App">
+      <Fakestore />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Fakestore.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+const Fakestore = () => {
+  const [store, setStore] = useState([]);
+  useEffect(() => {
+    fakeStore();
+  }, []);
+
+  const fakeStore = async () => {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const users = await response.json();
+
+    setStore(users.splice(0, 4));
+  };
+  //   fakeStore();
+
+  return (
+    <div>
+      <h1>Fake Store</h1>
+      <div className="mainContainer">
+        {store.map((values) => {
+          return (
+            <>
+              <div className="card">
+                <img src={values.image} alt="image" />
+                <div className="content">
+                  <h3>{values.title}</h3>
+                  <h4>Price:{values.price}</h4>
+                  <p>Rating:{values.rating.rate}</p>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Fakestore;
